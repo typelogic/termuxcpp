0af52bb2b32473819dbf256dbd42a1b5e5647eb3
typelogic <truthadjustr@gmail.com>
2019-09-16 15:36:49 +0800

    cv.wait_for retval

--- main.cpp
+++ main.cpp
@@ -18,13 +18,15 @@ void delay(int n) {
 
 void fn() {
   delay(3);
-  cv.notify_one();
+  //cv.notify_one();
   delay(9);
-  flag = true;
-  cv.notify_one();
+  //flag = true;
+  //cv.notify_one();
 }
 
 int main(int argc, char *argv[]) {
+  //bool y;
+  cv_status y;
   int a = 5;
   int b = 9;
   int m = a++ + b;
@@ -33,12 +35,29 @@ int main(int argc, char *argv[]) {
   thread t(fn);
   t.detach();
   // t.join();
-
+ 
   unique_lock<mutex> lck(mtx);
-  cv.wait_for(lck, chrono::seconds(20), [&]() {
+  y = cv.wait_for(
+    lck, 
+    chrono::seconds(20) 
+    /*, [&]() {
     cout << "[" << flag << "]\n";
     return flag;
-  });
+    }*/
+  );
+
+  // cout << y << endl;
+
+  switch (y) {
+  case cv_status::timeout: 
+  cout << "TO\n";
+  break;
+  case cv_status::no_timeout: 
+  cout << "NOTO\n";
+  break;
+  default:
+  cout << "?\n";
+  }
 
   cout << "\nBYE\n";
   return flag;
